a63577eeae156c76149dcc57349a4a9d174b1d41
kapadiamush <kapadiamush@gmail.com>
2017-04-03 00:52:24 -0700

    add functionality for getting lines for first and second half of nba

--- constants/urls.js
+++ constants/urls.js
@@ -6,9 +6,15 @@ const BASKETBALL = `${BASE}/basketball`;
 const SUFFIX = "2";
 
 const NFL_FULL_GAME = `${FOOTBALL}/nfl/${SUFFIX}`;
+const NFL_FIRST_HALF = `${FOOTBALL}/nfl-1st-half/${SUFFIX}`;
+const NFL_SECOND_HALF = `${FOOTBALL}/nfl-2nd-half/${SUFFIX}`;
+
 const NBA_FULL_GAME = `${BASKETBALL}/nba/${SUFFIX}`;
+const NBA_FIRST_HALF = `${BASKETBALL}/nba-1st-half/${SUFFIX}`;
+const NBA_SECOND_HALF = `${BASKETBALL}/nba-2nd-half/${SUFFIX}`;
 
 module.exports = {
-  NFL_FULL_GAME,
-  NBA_FULL_GAME
+  NBA_FULL_GAME,
+  NBA_FIRST_HALF,
+  NBA_SECOND_HALF
 };
--- nba.js
+++ nba.js
@@ -5,7 +5,11 @@ const getBets = require('./bets');
 const scrappers = require('./scrappers');
 
 const getFullGameBets = next => getBets(constants.URLS.NBA_FULL_GAME, scrappers.fullGameScrapper)(next);
+const getFirstHalfBets = next => getBets(constants.URLS.NBA_FIRST_HALF, scrappers.halfGameScrapper)(next);
+const getSecondHalfBets = next => getBets(constants.URLS.NBA_SECOND_HALF, scrappers.halfGameScrapper)(next);
 
 module.exports = {
-  getFullGameBets
+  getFullGameBets,
+  getFirstHalfBets,
+  getSecondHalfBets
 };
