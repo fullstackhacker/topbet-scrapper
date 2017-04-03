365c9cdfc9d72025bbf53fa72497412a6bafc783
kapadiamush <kapadiamush@gmail.com>
2017-04-03 00:49:21 -0700

    arror functions can't be used as constructors

--- exceptions.js
+++ exceptions.js
@@ -1,21 +1,23 @@
 "use strict";
 
-const ParseError = message => {
+const ParseError = function(message){
   this.message = message;
   this.stack = (new Error()).stack;
-};
+}
 
 ParseError.prototype = Object.create(Error.prototype);
 ParseError.prototype.name = "ParseError";
+ParseError.prototype.constructor = ParseError;
 
 
-const FetchError = message => {
+const FetchError = function(message) {
   this.message = message;
   this.stack = (new Error()).stack;
 };
 
 FetchError.prototype = Object.create(Error.prototype);
 FetchError.prototype.name = "FetchError";
+FetchError.prototype.constructor = FetchError;
 
 module.exports = {
   ParseError,
